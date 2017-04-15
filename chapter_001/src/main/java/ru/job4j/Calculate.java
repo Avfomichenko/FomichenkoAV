82e36da308d25a8a3857336884b1f0f2c8453d11
Alexandr Fomichenko <Avfomichenko@gmail.com>
2017-04-15 14:19:56 +0300

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,16 @@
 package ru.job4j;
-
-
-
-
+/**
+* Class  Класс для вывода на экран строки.
+* @author fomichenkoav	
+* @since 13.07.2017
+* @version 1
+*/
 public  class Calculate {
+	
+/**
+* Конструктор, вывод строки в консоль.
+* @param arg - arg.
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
