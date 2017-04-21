e4b1ebc7c78fd7f4cd3d95d240d2c16150081d61
Alexandr Fomichenko <Avfomichenko@gmail.com>
2017-04-21 22:10:06 +0300

    task 187

---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculateTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *Class Test.
+ *@author fomichenkoav
+ *@since 20.04.2017
+ *@version $Id$
+ */
+public  class CalculateTest {
+    /**
+     *Test add.
+     */
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+	
+	/**
+     *Test subtract.
+     */
+    @Test
+    public void whenAddTwoDeductOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.subtract(2D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+	/**
+     *Test div.
+     */
+    @Test
+    public void whenAddTwoDivideTwoThenOne() {
+        Calculator calc = new Calculator();
+        calc.div(2D, 2D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+	/**
+     *Test multiple.
+     */
+    @Test
+    public void whenAddTwoMultiplyTwoThenFour() {
+        Calculator calc = new Calculator();
+        calc.multiple(2D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+
+
+}
