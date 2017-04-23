fc1de2e2539b9789d8cdac416eeab6b4f48bceca
Alexandr Fomichenko <Avfomichenko@gmail.com>
2017-04-23 20:26:53 +0300

    task 189

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,26 @@
+package test.java.ru.job4j.max;
+
+
+import main.java.ru.job4j.max.Max;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Fomichenkoav.
+ * @version 1
+ * @since 23.04.2017
+ */
+public class MaxTest {
+    /**
+     *Test add.
+     */
+    @Test
+    public void maximumNumberOfFirstAndSecond() {
+        Max max = new Max();
+        int result =  max.max(4, 1);
+        int expected = 4;
+        assertThat(result, is(expected));
+    }
+}
+
