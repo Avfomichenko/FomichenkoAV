fe5a6af6d78e0f23d3f4b10c435b67f7efa70c0a
Alexandr Fomichenko <Avfomichenko@gmail.com>
2017-05-01 18:02:48 +0300

    task 188

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,31 @@
+package main.java.ru.job4j.condition;
+
+/**
+ * @author Fomichenkoav.
+ * @version 1
+ * @since 23.04.2017
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+
+        return this.y;
+    }
+
+    public boolean is(int a, int b) {
+
+        return y == a * x + b;
+    }
+
+}
---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Fomichenkoav.
+ * @version 1
+ * @since 23.04.2017
+ */
+package main.java.ru.job4j.condition;
