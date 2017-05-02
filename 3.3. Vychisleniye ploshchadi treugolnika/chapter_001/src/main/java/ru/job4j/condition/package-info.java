c6c5dab317578c85b208367c59b34c165c63a0c6
Alexandr Fomichenko <Avfomichenko@gmail.com>
2017-05-02 23:27:18 +0300

    task 188

---
+++ 3.3. Vychisleniye ploshchadi treugolnika/chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Fomichenkoav.
+ * @version 1
+ * @since 02.05.2017
+ */
+package test.java.ru.job4j.condition;
--- 3.3. Vychisleniye ploshchadi treugolnika/chapter_001/src/test/java/ru/job4j/max/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
-* Package for test task.
-* @author Fomichenko Alexandr (mailto:avfomichenko@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
-package test.java.ru.job4j.max;
