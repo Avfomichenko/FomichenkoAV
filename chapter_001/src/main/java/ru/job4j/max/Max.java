75dde2f1919ba97f851ac95f570e1729d9807d4e
Alexandr Fomichenko <Avfomichenko@gmail.com>
2017-05-12 16:51:29 +0300

    task 187

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,20 @@
+package main.java.ru.job4j.max;
+
+/**
+ *@author Fomichenkoav.
+ *@since 23.04.2017
+ *@version 1
+ */
+
+public class Max {
+    /**
+     * Тернарное условие, вычесление максимального значения.
+     *@return max
+	 *@param first  число
+	 *@param second число
+  */
+    public int max(int first, int second) {
+
+        return first < second ? second : first;
+		}
+}
